995f177d3d15325acd12241e017ee02d41cfe52a
Evan Omeje <evanomeje@gmail.com>
2025-03-17 01:07:13 -0600

    updated font family toolbar front end

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -64,22 +64,22 @@ const FontFamilyButton = () => {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <button 
-                 className= "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                 className= "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-full hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm border-2 border-black"
                 >
-                    <span className="truncate">
+                    <span className="truncate ">
                         {editor?.getAttributes("textStyle").fontFamily || "Arial"}
                     </span>
-                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0 rounded-full bg-black" />
                 </button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="p-1 flex flex-col gap-y-1 ">
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1 bg-[yellow] border-black border-2 rounded-none ">
                 {
                     fonts.map(({label, value}) => (
                         <button 
                         onClick = {() => editor?.chain().focus().setFontFamily(value).run()}
                         key={value}
                             className={cn(
-                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover-bg-neuatral-200/80",
+                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover-bg-neuatral-200/80 border-2 border-black bg-white",
                                 editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
                             )}
                             style= {{fontFamily: value}}
