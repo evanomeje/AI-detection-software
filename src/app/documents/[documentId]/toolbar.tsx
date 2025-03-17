5db6eca6a991d4981ade86cdb7f6738be37a2850
Evan Omeje <evanomeje@gmail.com>
2025-03-16 23:58:36 -0600

    added font/ font family feature, will edit front end later

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -11,12 +11,20 @@ import {
     UnderlineIcon, 
     MessageSquarePlusIcon,
     ListTodoIcon,
-    RemoveFormattingIcon
+    RemoveFormattingIcon,
+    ChevronDownIcon
         } from "lucide-react";
 import {cn} from "@/lib/utils";
 
 import { useEditorStore } from "@/store/use-editor-store";
 import { Separator } from "@/components/ui/separator";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface ToolbarButtonProps {
     onClick?: () => void;
@@ -33,13 +41,59 @@ const ToolbarButton = ( {
         <button
             onClick= {onClick}
             className={cn(
-                "test-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg- bg-neutral-200/80",
+                "w-9 h-9 flex items-center justify-center rounded-full hover:bg- bg-neutral-200/80 border-2 border-black",
                 isActive && "bg-neutral-200/80"
             )}>
             <Icon className= "size-6  "/>    
         </button>
     );
 }
+
+const FontFamilyButton = () => {
+    const {editor} = useEditorStore();
+
+    const fonts = [
+        { label: "Arial", value: "Arial" },
+        { label: "Courier New", value: "Courier New" },
+        { label: "Georgia", value: "Georgia" },
+        { label: "Times New Roman", value: "Times New Roman" },
+        { label: "Verdana", value: "Verdana" },
+    ];
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button 
+                 className= "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="truncate">
+                        {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1 ">
+                {
+                    fonts.map(({label, value}) => (
+                        <button 
+                        onClick = {() => editor?.chain().focus().setFontFamily(value).run()}
+                        key={value}
+                            className={cn(
+                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover-bg-neuatral-200/80",
+                                editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
+                            )}
+                            style= {{fontFamily: value}}
+                            >
+                                <span className="text-sm">{label}</span>
+
+                        </button>
+                    )
+                )}
+
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
 
 export const Toolbar = () => {
 
@@ -129,6 +183,8 @@ export const Toolbar = () => {
             ))}
         < Separator orientation="vertical" className="h-11 w-[3px] bg-black" />
         {/* TODO: FONT FAMILY*/}
+
+        <FontFamilyButton/> 
 
         < Separator orientation="vertical" className="h-11 w-[3px] bg-black" />
         {/* TODO: HEADING*/}
